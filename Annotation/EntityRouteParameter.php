1139253ed123c13e7bcfb6deaba90c4345b068dc
Aaron Muylaert <aaron@tactics.be>
2013-01-28 21:44:22 +0100

    Allow setting of a name for an EntityRouteParameter.

--- Annotation/EntityRouteParameter.php
+++ Annotation/EntityRouteParameter.php
@@ -4,7 +4,35 @@ namespace Tactics\Bundle\EntityRouteBundle\Annotation;
 
 /**
  * @Annotation
+ *
+ * @author Aaron Muylaert <aaron@tactics.be>
  */
 class EntityRouteParameter extends \Doctrine\Common\Annotations\Annotation
 {
+    /**
+     * @var string $name
+     */
+    protected $name;
+
+    /**
+     * Constructs a new instance of EntityRouteParameter.
+     *
+     * @param array $options The options.
+     */
+    public function __construct(array $options)
+    {
+        if (isset($options['name'])) {
+            $this->name = $options['name'];
+        }
+    }
+
+    /**
+     * Get name.
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
 }
